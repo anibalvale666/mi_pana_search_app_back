7ed2aca2a7730015062f34da3861e9c6b531711f
anibalvale666 <anibal.valenzuela@ucsp.edu.pe>
2021-10-04 13:12:52 -0500

    filter

--- controllers/data.js
+++ controllers/data.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 const {response} = require('express');
 const Data = require('../models/Data');
-=======
-const {response} = require('express')
-const Data = require('../models/Data')
->>>>>>> db313bb26f523cbd8811b068839f7d8c869b143a
 
 
 
